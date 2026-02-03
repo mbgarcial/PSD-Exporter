c7cdf9fb29360b9b68d1355d4a89baff385de006
M Beatriz Garcia <mbgarcial@ishtera.net>
2026-02-03 19:15:22 -0300

    delete older versions of files rename files

--- psd_funcs.py
+++ psd_funcs.py
@@ -5,153 +5,6 @@ from PIL import Image, ImageChops
 import re
 
 
-#------------------------
-# Unused yet
-    
-class PSDItem:
-    def __init__(self, parent, name, id=0, export = True, visible = True, ignore = False) -> None:
-        self.parent  = parent
-        self.name    = name
-        self.id      = id
-        self.export  = export
-        self.visible = visible
-        self.ignore  = ignore
-
-    def __repr__(self) -> str:
-        return f"PSD_Item({self.parent}, {self.name}, export = {self.export})"
-    
-    def __str__(self) -> str:
-        return self.name 
-    
-    # methods
-    def toggle_export(self, value:bool|None = None) -> None:
-        """Toggles export value"""
-        if value == None:
-            self.export = not self.export
-        else:
-            self.export = value
-
-    def skip(self) -> None:
-        """Sets export & ignore to False"""
-        self.ignore = True
-        self.export = False
-
-    def not_skip(self) -> None:
-        """Sets export & ignore to False"""
-        self.ignore = False
-        self.export = True
-
-    def export_check(self, ignore_invisible = False) -> str:
-        """returns what kind of exporting should be done to this item"""
-        if not self.visible and ignore_invisible:
-            return "skip"
-        
-        if self.export and not self.ignore:
-            return "export"
-        
-        else:
-            return "skip"
-    
-class PSDLayer(PSDItem):
-    def __init__(self, parent, name, id=0, export = True, visible = True, ignore = False) -> None:
-        super().__init__(parent, name, id, export, visible, ignore)
-
-    @classmethod
-    def make(cls, layer:Layer) -> PSDLayer:
-        l = PSDLayer(layer.parent, layer.name, layer.layer_id, visible = layer.visible)
-        if layer.clipping:
-            l.ignore = True
-        return l
-    
-class PSDFolder(PSDItem):
-    def __init__(self, parent, name, id=0, export = True, visible = True, ignore = False) -> None:
-        super().__init__(parent, name, id, export, visible, ignore)
-
-        self.contents = []
-    
-    @classmethod
-    def make(cls, group:Group) -> PSDFolder:
-        f = PSDFolder(group.parent, group.name, group.layer_id, visible = group.visible)
-        f.contents = [make_PSDItem(layer) for layer in group]
-        if group.clipping:
-            f.ignore = True
-        return f
-
-    # methods
-    def export_check(self, ignore_invisible = False) ->str:
-        """returns what kind of exporting should be done to this item"""
-        if not self.visible and ignore_invisible:
-            return "skip"
-        
-        if self.ignore:
-            return "skip"
-        elif self.export:
-            return "export"
-        else:
-            return "pass"
-
-    def add_item(self, item: PSDItem)->None:
-        item.parent = self.name
-        self.contents.append(item)
-    
-def make_PSDItem(layer:Layer):
-    if layer.is_group:
-        return PSDLayer.make(layer)
-    else:
-        return PSDFolder.make(layer) #type:ignore
-
-class ExportInfo():
-    def __init__(self, name, size=(0,0), keep_ratio = True, path = "", trim = False, crop_layer = None, scale = 1.0, prefix = "", parentprefix = False, format = "png", ignore_selected = False, ignore_prefix = "i-", export_prefix = ""):
-        self.name    = name # root folder name
-        self.path    = path # path to export
-        self.size    = size # canvas size in pixels, as a tuple 
-        self.keepratio = keep_ratio # if True, keep aspect ratio when resizing
-        self.trim    = trim # if True, export layers/folders with empty space trimmed. If False, export in full canvas size.
-        self.crop    = crop_layer # if not None, a str with the unique name of a layer to use as crop mask.
-        self.scale   = scale # scale to export this object
-        self.prefix  = prefix # a prefix (default is "") to add to all exported layers
-        self.parentprefix = parentprefix # if False, export each layer as it's named. Otherwise, add parent name as a prefix.
-        self.format  = format
-        self.ignoreselect = ignore_selected # if True, ignores all selected instead of exporting all selected
-        self.ignoreprefix = ignore_prefix # a str. All layers/folders with names that start with this str will be ignored.
-        self.exportprefix = export_prefix # a str. if it's not "", ONLY layers/folders with this prefix will be exported.
-
-    @property
-    def height(self) -> int:
-        return self.size[1]
-    
-    @height.setter
-    def height(self, value) -> None:
-        self.resize(height=value)
-    
-    @property
-    def width(self) -> int:
-        return self.size[0]
-    
-    @width.setter
-    def width(self, value) -> None:
-        self.resize(width=value)
-
-
-    def resize(self, width=None, height=None) -> None:
-        w,h = self.size
-        ratio = h/w
-
-        if not self.keepratio:
-            w = width if width else w
-            h = height if height else h
-
-        else:
-            if width:
-                w = width
-                h = int(ratio * w)
-            
-            elif height:
-                h = height
-                w = int(1/ratio * h)
-        
-        self.size = (w,h)
-            
 #--------------------------
 #  PSD Processing
 
@@ -200,16 +53,42 @@ class ExportableImg():
         # if this should be ignored, move on.
         if self.get_ignore():
             return True
-
+        
+        # get export args
+        export_args = self.get_args()
 
         # if this is a layer, make sure the layer is visible.
         if not isinstance(self.image, PSDImage):
             self.image.visible = True #type:ignore
         
         # convert to a pil image, using the export args
-        image = layer_to_img(self.image, **self.get_args())
+        image = layer_to_img(self.image, **export_args)
 
+        # if the image succeeded in being created, process and save
         if image:
+
+            # Scale it if we're doing that
+            if export_args["scale"] != 1.0:
+                
+                if export_args["scale"] > 5.0:
+                    raise ValueError("Sorry, scale is way too big! (how did we get here???)")
+                    
+                #if we're keeping aspect ratio:
+                if export_args["keep_aspect_ratio"]:
+                    new_size = tuple(int(i*export_args["scale"]) for i in image.size)
+                
+                #if not..
+                else:
+                    og_size = export_args["canvas_size"]
+                    w_scale = get_width_scale(og_size,int(export_args["width"]))
+                    h_scale = get_height_scale(og_size,int(export_args["height"]))
+                    print("og size: ", og_size, " image size: ", image.size, "saved W x H: ", export_args["width"], "x", export_args["height"])
+                    new_size = tuple([int(round(w_scale*image.size[0])),int(round(h_scale*image.size[1]))])
+                
+                
+                image = image.resize(new_size, Image.Resampling.LANCZOS) #type: ignore
+
+            # finally, save it
             image.save(self.get_savepath())
 
         # return visibility to what it was originally
@@ -267,9 +146,6 @@ def process_psd(psd:PSDImage|Group, name, path, **kwargs) -> bool | str:
                 # convert the mask to an alpha b/w image and store it
                 group_kwargs["alpha"] = create_alpha(mask, group_kwargs.get("canvas_size", (mask.bbox[2],mask.bbox[3])))
                 
-                # ⚠️ TEST
-                # group_kwargs["alpha"].save(new_path+"/"+group_name+"_mask.png")
-
                 # if we don't have a bbox, save the bbox as the mask bbox. We'll trim later when exporting.
                 if not group_kwargs.get("bbox", None):
                     group_kwargs["bbox"] = mask.bbox # type: ignore
@@ -305,17 +181,24 @@ def should_ignore_layer(layer:Layer,**kwargs):
 # Image Creation
 
 def layer_to_img(layer:Layer|PSDImage, **kwargs) -> Image.Image | None:
-    """Evaluates the layer and returns a pil image. Returns None if the image creation fails"""   
+    """Composites a Layer/PSDImage and returns an image. Returns None if the image creation fails"""   
+    
+    # Retrieve some pertinent arguments
+    trim_layers  = kwargs.get("trim_layers", True) # Are we trimming pixels?
+    bbox         = kwargs.get("bbox", layer.bbox) # bounding box to crop the Layer. Default is the layer's own bbox 
+    alpha        = kwargs.get("alpha", None) # alpha mask image. Only exists if layer is inside a group that has a mask.
+    trim_to_mask = kwargs.get("trim_to_mask", False) # if we're cropping layers to their mask's bbox.
+    canvas_size  = kwargs.get("canvas_size",(bbox[2],bbox[3]))
     
     # If we got a PSDImage, flatten it and return.
     if isinstance(layer,PSDImage):
-        image = layer.composite(force = True)
 
-    # Retrieve some pertinent arguments
-    bbox         = kwargs.get("bbox", layer.bbox) # bounding box. Default is the layer's own bbox 
-    alpha        = kwargs.get("alpha", None) # alpha mask image. Only exists if layer is inside a group that has a mask.
-    trim_to_mask = kwargs.get("trim_to_mask", False) # if we're cropping layers to their mask's bbox.
+        # if we're trimming, bbox is psd visible layers bbox, otherwise is canvas size.
+        bbox = layer.bbox if trim_layers else tuple([0,0] + list(layer.size)) # don't use canvas_size unless we're cropping!
 
+        return layer.composite(force = True, viewport=bbox) #type: ignore
+        
+    
     if not alpha:
         
         # If this layer has a mask and we're trimming to mask, make the mask's bbox the layer's bbox
@@ -326,20 +209,30 @@ def layer_to_img(layer:Layer|PSDImage, **kwargs) -> Image.Image | None:
         return layer.composite(viewport=bbox)
         
     # composites with alpha
-    image = composite_alpha(layer,alpha,canvas_size=kwargs.get("canvas_size",(bbox[2],bbox[3])))
+    image = composite_alpha(layer,alpha,canvas_size)
 
     # Trim the image if we're not trimming to mask and we don't have trimming disabled.
-    if not trim_to_mask and kwargs.get("trim_layers", True):
+    if not trim_to_mask and trim_layers:
         bbox  = image.getbbox()  # type:ignore
 
     # return cropped image!   
     return image.crop(bbox) # type:ignore
 
-def composite_alpha(layer,alpha,canvas_size)->Image.Image|None: 
+def create_alpha(mask, canvas_size) -> Image.Image|None:
+    """converts a layer mask to b/w image to save on alpha channel"""
+
+    og_mask  = mask.topil().convert("L") #type: ignore
+    new_mask = Image.new("RGB", canvas_size , color=(0,0,0)).convert("L")
+
+    Image.Image.paste(new_mask,og_mask,(mask.bbox[0],mask.bbox[1]))
+
+    return new_mask
+
+def composite_alpha(layer,alpha,canvas_size) -> Image.Image|None: 
     """Composites layer and applies an alpha mask image to it"""
 
     # Alpha processing: 
-    viewport    = tuple([0,0,canvas_size[0], canvas_size[1]])
+    viewport    = tuple([0,0] + list(canvas_size))
     image       = layer.composite(force = True, viewport = viewport) #type: ignore
 
     # split image into channels to get the layer's mask/alpha channel
@@ -353,16 +246,22 @@ def composite_alpha(layer,alpha,canvas_size)->Image.Image|None:
 
     return image
 
-def create_alpha(mask, canvas_size):
-    """returns mask image to save on alpha channel"""
+def get_height_scale(og_size,new_height)->float:
+    """returns height scale"""
+    return get_scale(og_size,height=new_height)
 
-    og_mask  = mask.topil().convert("L") #type: ignore
-    new_mask = Image.new("RGB", canvas_size , color=(0,0,0)).convert("L")
-
-    Image.Image.paste(new_mask,og_mask,(mask.bbox[0],mask.bbox[1]))
-
-    return new_mask
-
+def get_width_scale(og_size,new_width)->float:
+    """returns width scale"""
+    return get_scale(og_size,width=new_width)
+    
+def get_scale(size:tuple[int,int], width=None, height=None)-> float:
+    """returns a float that corresponds what % of the og width/heigh is the given width/height"""
+    ogw, ogh = size
+    if width:
+        return width/ogw
+    elif height:
+        return height/ogh
+    return 1.0
 #--------------------
 # GUI Integration
 
@@ -408,7 +307,7 @@ def get_layer_repr(layers:list[Layer], level=0)->str:
 #---------------------------
 # PSD opening on GUI
 
-def read_psd(filename:str) -> dict:
+def open_psd(filename:str) -> dict:
     """
     This function opens a PSD file from a path str and returns a dict with a PSDImage object and its name (filename.psd).
     If it fails, it returns an empty dict, signaling its failure.
@@ -427,6 +326,7 @@ def read_psd(filename:str) -> dict:
         return {}
     
     psd_dict["psd_name"] = get_psd_filename_from_path(filename)
+    psd_dict["psd_size"] = psd_dict["psd"].size
 
     return psd_dict
 
@@ -449,14 +349,18 @@ def get_export_args(**kwargs) -> dict:
         "trim_to_visible"   : False,
         "scale"             : 1.0,
         "bbox"              : None,
-        "ignored"           : []
+        "ignored"           : [],
+        "keep_aspect_ratio" : True,
+        "kind"              : "%",
+        "height"            : 0,
+        "width"             : 0
     }
 
     # make a new dict by replacing default values with kwargs we got
     args = {k:kwargs.get(k,default[k]) for k in default}
 
     # record the canvas size for future reference
-    args["canvas_size"] = kwargs["file"]["psd"].size #type: ignore
+    args["canvas_size"] = kwargs["file"]["psd_size"] #type: ignore
     
     # Define the bbox if we're not trimming empty pixels
     if not args["trim_layers"]:
@@ -467,7 +371,7 @@ def get_export_args(**kwargs) -> dict:
         
         # otherwise, bbox is document size.
         else:
-            args["bbox"] = tuple([0,0,args["canvas_size"][0],args["canvas_size"][1]]) #type:ignore
+            args["bbox"] = tuple([0,0] + list(args["canvas_size"])) #type:ignore
             
     return args
 
